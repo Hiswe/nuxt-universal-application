e8f9091798d22c43b04d8505dd3d01283656b92e
Hiswe <hiswehalya@gmail.com>
2018-10-15 13:46:48 +0700

    SERVER – update code to reflect create-nuxt-app
    https://github.com/nuxt/create-nuxt-app/blob/master/template/server/index-koa.js

--- server/index.js
+++ server/index.js
@@ -60,10 +60,16 @@ async function startServer() {
   }
 
   app.use(ctx => {
-    ctx.status = 200
-    ctx.respond = false // Mark request as handled for Koa
-    ctx.req.ctx = ctx // This might be useful later on, e.g. in nuxtServerInit or with nuxt-stash
-    nuxt.render(ctx.req, ctx.res)
+    ctx.status = 200 // koa defaults to 404 when it sees that status is unset
+
+    return new Promise((resolve, reject) => {
+      ctx.res.on('close', resolve)
+      ctx.res.on('finish', resolve)
+      nuxt.render(ctx.req, ctx.res, promise => {
+        // nuxt.render passes a rejected promise into callback on error.
+        promise.then(resolve).catch(reject)
+      })
+    })
   })
 
   app.listen(PORT, HOST, function endInit() {
